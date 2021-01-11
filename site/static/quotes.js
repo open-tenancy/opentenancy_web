2d466ecf0cd6b00c44c1c77fffd8ff8072b11038
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2021-01-11 22:23:04 +0000

    quick fix attempting changing quotes

---
+++ site/static/quotes.js
@@ -0,0 +1,17 @@
+"use strict"
+
+var quotes = ["Problems with your landlord?", "Has your water stopped working?", "No heating in your flat?", "Flat filled with pests?", "No hot water?"];
+
+writeQuote();
+
+function writeQuote() {
+  var quote;
+  quote=document.getElementsByClassName("quote")[0]; //will change as ian doesnt like this, but for now
+  quote.innerHTML = quotes[randomNum()]; //find a way to include into page/innerText maybes?
+}
+
+function randomNum() {
+  var curr_quote;
+  curr_quote = Math.floor((Math.random() * (quotes.length)));
+  return curr_quote;
+}
