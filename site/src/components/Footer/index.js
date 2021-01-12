a906fc6469617f6e314e0bd58b233889a1cfa862
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2021-01-12 22:19:55 +0000

    added the newsletter to the navbar

--- site/src/components/Footer/index.js
+++ site/src/components/Footer/index.js
@@ -6,8 +6,9 @@ const Footer = () => {
     <footer className='footer'>
       <div className='container'>
         <div className='content has-text-centered'>
+          <p><a href = 'https://mailchi.mp/c6aaa91d8ea3/register'>Newsletter</a> | <a href='https://www.buymeacoffee.com/opentenancy'> Buy us a Byte </a></p>
           <p><a href='/pricing'>Get Advice</a> | <a href='/blog'>Blog</a> | <a href='/contribute'>Contribute</a> | <a href='/about'>About</a> | <a href='/team'>The Team</a> | <a href='/sponsor'>Sponsors</a></p>
-          <p><a href='/about'>FAQ</a> | <a href='/privacy'>Privacy Policy</a> | <a href='https://www.buymeacoffee.com/opentenancy'> Buy us a Byte</a> </p>
+          <p><a href='/about'>FAQ</a> | <a href='/privacy'>Privacy Policy</a> </p>
           <p>
             Copyright © Amy Conroy, Anastasia Shmyglia 2021. All Rights Reserved.
           </p>
