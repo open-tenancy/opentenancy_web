c4ecf03a1b1d28c7ae2fe23a06af0c307f77a4e3
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2021-01-12 10:04:57 +0000

    attempted to fix quote gen

--- site/static/quotes.js
+++ site/static/quotes.js
@@ -6,8 +6,8 @@ writeQuote();
 
 function writeQuote() {
   var quote;
-  quote=document.getElementsByClassName("quote")[0]; //will change as ian doesnt like this, but for now
-  quote.innerHTML = quotes[randomNum()]; //find a way to include into page/innerText maybes?
+    quote=document.getElementsByClassName("quote")[0]; //will change as ian doesnt like this, but for now
+    quote.innerHTML = quotes[randomNum()]; //find a way to include into page/innerText maybes?
 }
 
 function randomNum() {
